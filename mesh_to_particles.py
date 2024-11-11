ce026856aa9f1bef7ff076f9e1278472a250c71b
David Pershouse <n11020211@qut.edu.au>
2024-11-11 11:28:27 +1000

    extract particle mesh helpers

---
+++ mesh_to_particles.py
@@ -0,0 +1,91 @@
+import open3d as o3d
+import numpy as np
+import pymeshfix
+from initializerdefs import ParticlesSpatialDef
+from typing import List
+
+def clean_and_watertight_mesh(mesh: o3d.geometry.TriangleMesh) -> o3d.geometry.TriangleMesh:
+    # remove floating parts
+    # Get connected components of the mesh
+    #triangle_clusters, cluster_n_triangles, _ = mesh.cluster_connected_triangles()
+    #triangle_clusters = np.asarray(triangle_clusters)
+    #cluster_n_triangles = np.asarray(cluster_n_triangles)
+    
+    # Find the largest cluster
+    #largest_cluster_idx = cluster_n_triangles.argmax()
+    
+    # Create a mask for triangles in the largest cluster
+    #triangles_to_remove = triangle_clusters != largest_cluster_idx
+    #mesh.remove_triangles_by_mask(triangles_to_remove)
+    
+    # Remove isolated vertices
+    #mesh.remove_unreferenced_vertices()
+
+    mesh = mesh.filter_smooth_laplacian(number_of_iterations=5)
+
+    v = np.array(mesh.vertices)
+    t = np.array(mesh.triangles)
+    nv, nt = pymeshfix.clean_from_arrays(v,t)
+    clean_mesh = o3d.geometry.TriangleMesh()
+    clean_mesh.vertices = o3d.utility.Vector3dVector(nv)
+    clean_mesh.triangles = o3d.utility.Vector3iVector(nt)
+
+    return clean_mesh #.filter_smooth_laplacian(number_of_iterations=1)
+
+
+# creates particles from mesh, the resulting shape is hollow
+def match_mesh_with_particles(mesh: o3d.geometry.TriangleMesh, particle_radius: float) -> List[ParticlesSpatialDef]:
+    voxel_grid = o3d.geometry.VoxelGrid.create_from_triangle_mesh(mesh, particle_radius*2)
+    particles = []
+    for voxel in voxel_grid.get_voxels():
+        particle = ParticlesSpatialDef(
+            xyz = np.array([
+                float(voxel.grid_index[0] * voxel_grid.voxel_size + voxel_grid.origin[0]),
+                float(voxel.grid_index[1] * voxel_grid.voxel_size + voxel_grid.origin[1]), 
+                float(voxel.grid_index[2] * voxel_grid.voxel_size + voxel_grid.origin[2])
+            ]),
+            radius = particle_radius
+        )
+        particles.append(particle)
+    return particles
+
+
+# creates particles from mesh, the resulting shape is solid, uses a dense voxel grid
+def fill_mesh_with_particles(mesh: o3d.geometry.TriangleMesh, particle_radius: float) -> List[ParticlesSpatialDef]:
+    # Create a dense voxel grid from mesh bounds
+    mesh_min = np.asarray(mesh.get_min_bound())
+    mesh_max = np.asarray(mesh.get_max_bound())
+    voxel_size = particle_radius * 2
+    
+    # Calculate grid dimensions
+    dims = np.ceil((mesh_max - mesh_min) / voxel_size).astype(int)
+    print(f"Dims: {dims}")
+    
+    # Create grid points
+    x = np.arange(0, dims[0]) * voxel_size + mesh_min[0]
+    y = np.arange(0, dims[1]) * voxel_size + mesh_min[1] 
+    z = np.arange(0, dims[2]) * voxel_size + mesh_min[2]
+    
+    xx, yy, zz = np.meshgrid(x, y, z)
+    points = np.stack([xx.flatten(), yy.flatten(), zz.flatten()], axis=1).astype(np.float32)
+
+    scene = o3d.t.geometry.RaycastingScene()
+    scene.add_triangles(o3d.t.geometry.TriangleMesh.from_legacy(mesh))
+    
+
+
+    # Check which points are inside mesh
+    inside = scene.compute_occupancy(points).numpy().astype(bool)
+    # 1 is inside, 0 is outside, get the list of inside_points
+    inside_points = points[inside]
+
+    # Create particles for inside points
+    particles = []
+    for point in inside_points:
+        particle = ParticlesSpatialDef(
+            xyz = point,
+            radius = particle_radius
+        )
+        particles.append(particle)
+        
+    return particles
--- particle_builder.py
+++ particle_builder.py
@@ -14,7 +14,7 @@ import cv2
 import open3d as o3d
 import torch
 import pointops
-import pymeshfix
+from mesh_to_particles import clean_and_watertight_mesh, fill_mesh_with_particles
 
 import logging
 logger = logging.getLogger("sam3d-builder")
@@ -207,93 +207,11 @@ def get_mesh_for_group(mesh: o3d.geometry.TriangleMesh, pcd_dict: LabelledPcd, g
     object_mesh = mesh.select_by_index(indices[mask_dis])
 
     #object_mesh = mesh.select_by_index(indices)
-    return clean_mesh(object_mesh)
-
-def clean_mesh(mesh: o3d.geometry.TriangleMesh) -> o3d.geometry.TriangleMesh:
-    # remove floating parts
-    # Get connected components of the mesh
-    #triangle_clusters, cluster_n_triangles, _ = mesh.cluster_connected_triangles()
-    #triangle_clusters = np.asarray(triangle_clusters)
-    #cluster_n_triangles = np.asarray(cluster_n_triangles)
-    
-    # Find the largest cluster
-    #largest_cluster_idx = cluster_n_triangles.argmax()
-    
-    # Create a mask for triangles in the largest cluster
-    #triangles_to_remove = triangle_clusters != largest_cluster_idx
-    #mesh.remove_triangles_by_mask(triangles_to_remove)
-    
-    # Remove isolated vertices
-    #mesh.remove_unreferenced_vertices()
-
-    mesh = mesh.filter_smooth_laplacian(number_of_iterations=5)
-
-    v = np.array(mesh.vertices)
-    t = np.array(mesh.triangles)
-    nv, nt = pymeshfix.clean_from_arrays(v,t)
-    clean_mesh = o3d.geometry.TriangleMesh()
-    clean_mesh.vertices = o3d.utility.Vector3dVector(nv)
-    clean_mesh.triangles = o3d.utility.Vector3iVector(nt)
-
-    return clean_mesh #.filter_smooth_laplacian(number_of_iterations=1)
+    return clean_and_watertight_mesh(object_mesh)
 
 
-# creates particles from mesh, the resulting shape is hollow
-def match_mesh_with_particles(mesh: o3d.geometry.TriangleMesh) -> List[ParticlesSpatialDef]:
-    voxel_grid = o3d.geometry.VoxelGrid.create_from_triangle_mesh(mesh, PARTICLE_RADIUS*2)
-    particles = []
-    for voxel in voxel_grid.get_voxels():
-        particle = ParticlesSpatialDef(
-            xyz = np.array([
-                float(voxel.grid_index[0] * voxel_grid.voxel_size + voxel_grid.origin[0]),
-                float(voxel.grid_index[1] * voxel_grid.voxel_size + voxel_grid.origin[1]), 
-                float(voxel.grid_index[2] * voxel_grid.voxel_size + voxel_grid.origin[2])
-            ]),
-            radius = PARTICLE_RADIUS
-        )
-        particles.append(particle)
-    return particles
 
 
-# creates particles from mesh, the resulting shape is solid, uses a dense voxel grid
-def fill_mesh_with_particles(mesh: o3d.geometry.TriangleMesh) -> List[ParticlesSpatialDef]:
-    # Create a dense voxel grid from mesh bounds
-    mesh_min = np.asarray(mesh.get_min_bound())
-    mesh_max = np.asarray(mesh.get_max_bound())
-    voxel_size = PARTICLE_RADIUS * 2
-    
-    # Calculate grid dimensions
-    dims = np.ceil((mesh_max - mesh_min) / voxel_size).astype(int)
-    print(f"Dims: {dims}")
-    
-    # Create grid points
-    x = np.arange(0, dims[0]) * voxel_size + mesh_min[0]
-    y = np.arange(0, dims[1]) * voxel_size + mesh_min[1] 
-    z = np.arange(0, dims[2]) * voxel_size + mesh_min[2]
-    
-    xx, yy, zz = np.meshgrid(x, y, z)
-    points = np.stack([xx.flatten(), yy.flatten(), zz.flatten()], axis=1).astype(np.float32)
-
-    scene = o3d.t.geometry.RaycastingScene()
-    scene.add_triangles(o3d.t.geometry.TriangleMesh.from_legacy(mesh))
-    
-
-
-    # Check which points are inside mesh
-    inside = scene.compute_occupancy(points).numpy().astype(bool)
-    # 1 is inside, 0 is outside, get the list of inside_points
-    inside_points = points[inside]
-
-    # Create particles for inside points
-    particles = []
-    for point in inside_points:
-        particle = ParticlesSpatialDef(
-            xyz = point,
-            radius = PARTICLE_RADIUS
-        )
-        particles.append(particle)
-        
-    return particles
 
 
 def extract_object_and_table_groups(segmented_cloud: LabelledPcd, dataset: StaticDataset) -> Tuple[List[int], int]:
@@ -325,26 +243,13 @@ def initialize_scene(dataset: StaticDataset, scene: Optional[SceneSetup] = None,
     
     logger.info(f"Segmented cloud has {np.unique(segmented_cloud['group'])} unique groups - {np.unique(segmented_cloud['group'])}")
 
-    # ignore groups that are in less than framecount -1 frames
-    visibilitiy = []
-    for f in dataset.frames:
-        groups = groups_in_view(segmented_cloud, f)
-        visibilitiy.extend(groups)
+    # TODO: remove points on wrong side of table plane
+    object_meshes = get_object_meshes(segmented_cloud, dataset)
 
-    all_groups = np.unique(segmented_cloud["group"])
-    thresh = len(dataset.frames) - 1
-    valid_groups = [g for g in all_groups if visibilitiy.count(g) >= thresh and g >= 0]
-    logger.info(f"Valid groups: {valid_groups}")
+    # create particles from meshes
+    object_particles = [fill_mesh_with_particles(mesh, PARTICLE_RADIUS) for mesh in object_meshes]
 
-    # remove the table which will be the largest group
-    group_points = segmented_cloud["group"]
     
-    # Find most frequent group from valid_groups
-    unique, counts = np.unique(group_points, return_counts=True)
-    freq_dict = dict(zip(unique, counts))
-    table_group = max((freq_dict[g], g) for g in valid_groups)[1]
-
-    logger.info(f"Table group: {table_group}")
 
 
 
